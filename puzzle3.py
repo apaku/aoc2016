c17b2613cfd4446ddd8cc269ea527ef9862e8e99
Andreas Pakulat <andreas@froglogic.com>
2017-12-28 11:57:00 +0100

    Some simple code-reuse

--- puzzle3.py
+++ puzzle3.py
@@ -1,20 +1,21 @@
 import sys
 
+def istriangle(listofthree):
+    (a,b,c) = tuple(listofthree)
+    return a+b > c and a+c > b and b+c > a
+
 def part1(triangles):
     for triangle in triangles:
-        (a,b,c) = tuple(map(int, filter(bool, triangle.split(' '))))
-        if a+b > c and a+c > b and b+c > a:
-            yield (a,b,c)
+        yield istriangle(map(int, filter(bool, triangle.split(' '))))
 
 def part2(lines):
     intlines = [map(int, filter(bool, line.split(' '))) for line in lines]
     for i in range(0, len(intlines), 3):
         triangles = list(map(list, zip(*intlines[i:i+3])))
         for triangle in triangles:
-            (a,b,c) = tuple(triangle)
-            if a+b >c and a+c > b and b+c > a:
-                yield (a,b,c)
+            yield istriangle(triangle)
+
 if __name__ == "__main__":
     triangles = sys.stdin.readlines()
-    print len(list(part1(triangles)))
-    print len(list(part2(triangles)))
+    print len(filter(bool, part1(triangles)))
+    print len(filter(bool, part2(triangles)))
